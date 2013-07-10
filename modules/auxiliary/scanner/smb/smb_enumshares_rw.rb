e96e3d04b7890fce6afd0d3ef1311cf1802d47c8
sinn3r <wei_chen@rapid7.com>
2013-07-10 16:49:37 -0500

    Initial commit of the modified version

--- modules/auxiliary/scanner/smb/smb_enumshares_rw.rb
+++ modules/auxiliary/scanner/smb/smb_enumshares_rw.rb
@@ -2,12 +2,11 @@
 # This file is part of the Metasploit Framework and may be subject to
 # redistribution and commercial restrictions. Please see the Metasploit
 # Framework web site for more information on licensing and terms of use.
-# http://metasploit.com/framework/
+#   http://metasploit.com/framework/
 ##
 
-
 require 'msf/core'
-
+require 'msf/core/auxiliary/report'
 
 class Metasploit3 < Msf::Auxiliary
 
@@ -19,45 +18,51 @@ class Metasploit3 < Msf::Auxiliary
 	include Msf::Auxiliary::Report
 	include Msf::Auxiliary::Scanner
 
-	def initialize
-		super(
-			'Name'        => 'SMB Share Enumeration',
-			'Description' => 'Determine what shares are provided by the SMB service and which ones are readable/writable',
-			'Author'      =>
+	def initialize(info={})
+		super(update_info(info,
+			'Name'           => 'SMB Share Enumeration',
+			'Description'    => %q{
+				This module determines what shares are provided by the SMB service and which ones
+				are readable/writable. It also collects additional information such as share types,
+				files, writable paths, etc.
+			},
+			'Author'         =>
 				[
 					'hdm',
 					'nebulus'
 				],
+			'License'        => MSF_LICENSE,
+			'DefaultOptions' =>
+				{
+					'DCERPC::fake_bind_multi' => false
+				}
+		))
 
-			'License'     => MSF_LICENSE,
-			'DefaultOptions' => {
-				'DCERPC::fake_bind_multi' => false
-			}
-		)
 		deregister_options('RPORT', 'RHOST')
 	end
 
 	def share_type(val)
-		[
-			'DISK',
-			'PRINTER',
-			'DEVICE',
-			'IPC',
-			'SPECIAL',
-			'TEMPORARY'
-		][val]
+		[ 'DISK', 'PRINTER', 'DEVICE', 'IPC', 'SPECIAL', 'TEMPORARY' ][val]
 	end
 
 	def device_type_int_to_text(device_type)
-		types = ["UNSET", "BEEP", "CDROM", "CDROM FILE SYSTEM", "CONTROLLER", "DATALINK",
+		types = [
+			"UNSET", "BEEP", "CDROM", "CDROM FILE SYSTEM", "CONTROLLER", "DATALINK",
 			"DFS", "DISK", "DISK FILE SYSTEM", "FILE SYSTEM", "INPORT PORT", "KEYBOARD",
 			"MAILSLOT", "MIDI IN", "MIDI OUT", "MOUSE", "UNC PROVIDER", "NAMED PIPE",
 			"NETWORK", "NETWORK BROWSER", "NETWORK FILE SYSTEM", "NULL", "PARALLEL PORT",
 			"PHYSICAL NETCARD", "PRINTER", "SCANNER", "SERIAL MOUSE PORT", "SERIAL PORT",
 			"SCREEN", "SOUND", "STREAMS", "TAPE", "TAPE FILE SYSTEM", "TRANSPORT", "UNKNOWN",
 			"VIDEO", "VIRTUAL DISK", "WAVE IN", "WAVE OUT", "8042 PORT", "NETWORK REDIRECTOR",
-			"BATTERY", "BUS EXTENDER", "MODEM", "VDM"]
-		return types[device_type]
+			"BATTERY", "BUS EXTENDER", "MODEM", "VDM"
+		]
+
+		types[device_type]
+	end
+
+	def to_unix_time(thi, tlo)
+		t = ::Time.at(::Rex::Proto::SMB::Utils.time_smb_to_unix(thi, tlo))
+		t.strftime("%m-%d-%Y %H:%M:%S")
 	end
 
 	def eval_host(ip, share)
@@ -69,50 +74,49 @@ class Metasploit3 < Msf::Auxiliary
 		begin
 			device_type = self.simple.client.queryfs_fs_device['device_type']
 
-			if(not device_type)
+			unless device_type
 				print_error("\\\\#{ip}\\#{share}: Error querying filesystem device type")
 				return false,false,nil,nil
 			end
-			rescue ::Rex::Proto::SMB::Exceptions::ErrorCode => e
-				if(e.to_s =~ /The server responded with error: 0xffff0002/)
-				# 0xffff0002 means that the server can't handle the request for device type
-					device_type=-1
-				elsif(	e.to_s =~ /The server responded with error: STATUS_INVALID_DEVICE_REQUEST/)
-					return false,false,"Invalid device request"
-				elsif(	e.to_s =~ /The server responded with error: 0x00040002/ )
-					return false,false,"Mac/Apple Clipboard?"
 
-				elsif(	e.to_s =~ /The server responded with error: STATUS_NETWORK_ACCESS_DENIED/ or
-					e.to_s =~ /The server responded with error: 0x00030001/ or
-					e.to_s =~ /The server resposded with error: 0x00060002/
-				)
-				#0x0006002 = bad network name, 0x0030001 Directory not found
-					return false,false,nil,nil
-				else
-					print_error("\\\\#{ip}\\#{share}: Error querying filesystem device type (#{e})")
-					return false,false,nil,nil
-				end
+		rescue ::Rex::Proto::SMB::Exceptions::ErrorCode => e
+			err = e.to_s.scan(/The server responded with error: (\w+)/i).flatten[0]
+			case err
+			when /0xffff0002/
+				# 0xffff0002 means that the server can't handle the request for device type
+				device_type = -1
+			when /STATUS_INVALID_DEVICE_REQUEST/
+				return false,false,"Invalid device request"
+			when /0x00040002/
+				return false,false,"Mac/Apple Clipboard?"
+			when /STATUS_NETWORK_ACCESS_DENIED/, /0x00030001/, /0x00060002/
+				# 0x0006002 = bad network name, 0x0030001 Directory not found
+				return false,false,nil,nil
+			else
+				print_error("\\\\#{ip}\\#{share}: Error querying filesystem device type")
+				return false,false,nil,nil
+			end
 		end
 
 		skip = false
 		msg = ''
 		case device_type
-			when -1
-				msg = "Unable to determine device"
-			when 1, 21 .. 29, 34 .. 35, 37 .. 44
-				skip = true
-				msg = "Unhandled Device Type (#{device_type})"
-			when 2 .. 16, 18 .. 20, 30 .. 33, 36
-				msg = device_type_int_to_text(device_type)
-			when 17
-				skip = true
-				msg = device_type_int_to_text(device_type)
-			else
-				msg = "Unknown Device Type"
-				msg << " (#{device_type})" if device_type
+		when -1
+			msg = "Unable to determine device"
+		when 1, 21 .. 29, 34 .. 35, 37 .. 44
+			skip = true
+			msg = "Unhandled Device Type (#{device_type})"
+		when 2 .. 16, 18 .. 20, 30 .. 33, 36
+			msg = device_type_int_to_text(device_type)
+		when 17
+			skip = true
+			msg = device_type_int_to_text(device_type)
+		else
+			msg = "Unknown Device Type"
+			msg << " (#{device_type})" if device_type
 		end
 
-		return read,write,msg,nil if(skip)
+		return read,write,msg,nil if skip
 
 		rfd = self.simple.client.find_first("\\")
 		read = true if rfd != nil
@@ -122,177 +126,179 @@ class Metasploit3 < Msf::Auxiliary
 		wfd.close
 		simple.delete("\\#{filename}")
 		simple.disconnect("\\\\#{ip}\\#{share}")
-		write = true # Operating under assumption STATUS_ACCESS_DENIED or the like will get thrown before write=true
+
+		# Operating under assumption STATUS_ACCESS_DENIED or the like will get
+		# thrown before write=true
+		write = true
 
 		return read,write,msg,rfd
 
 		rescue ::Rex::Proto::SMB::Exceptions::NoReply,::Rex::Proto::SMB::Exceptions::InvalidType,
 			::Rex::Proto::SMB::Exceptions::ReadPacket,::Rex::Proto::SMB::Exceptions::ErrorCode
 			return read,false,msg,rfd
+
 		rescue ::Exception => e
-			print_error("Error: '#{ip}' '#{e.class}' '#{e}'")
-	end # eval host
+			print_error("Error: '#{ip}' '#{e.class}' '#{e.to_s}'")
+	end
 
-	def run_host(ip)
+	def get_shares(ip, rport, info)
+		shares = []
 
-		found = false
-		[[139, false], [445, true]].each do |info|
+		res = self.simple.client.trans(
+			"\\PIPE\\LANMAN",
+			(
+				[0x00].pack('v') +
+				"WrLeh\x00"   +
+				"B13BWz\x00"  +
+				[0x01, 65406].pack("vv")
+			))
 
-		datastore['RPORT'] = info[0]
-		datastore['SMBDirect'] = info[1]
+		lerror, lconv, lentries, lcount = res['Payload'].to_s[
+			res['Payload'].v['ParamOffset'],
+			res['Payload'].v['ParamCount']
+		].unpack("v4")
 
-		begin
-			connect
-			smb_login
+		data = res['Payload'].to_s[
+			res['Payload'].v['DataOffset'],
+			res['Payload'].v['DataCount']
+		]
 
-			res = self.simple.client.trans(
-				"\\PIPE\\LANMAN",
-				(
-					[0x00].pack('v') +
-					"WrLeh\x00"   +
-					"B13BWz\x00"  +
-					[0x01, 65406].pack("vv")
-				)
-			)
+		0.upto(lentries - 1) do |i|
+			sname,tmp = data[(i * 20) +  0, 14].split("\x00")
+			stype     = data[(i * 20) + 14, 2].unpack('v')[0]
+			scoff     = data[(i * 20) + 16, 2].unpack('v')[0]
+			scoff -= lconv if lconv != 0
+			scomm,tmp = data[scoff, data.length - scoff].split("\x00")
+			shares << [ sname, share_type(stype), scomm]
+		end
 
-			shares = []
+		report_note(
+			:host   => ip,
+			:proto  => 'tcp',
+			:port   => rport,
+			:type   => 'smb.shares',
+			:data   => { :shares => shares.inspect },
+			:update => :unique_data
+		) unless shares.empty?
 
-			lerror, lconv, lentries, lcount = res['Payload'].to_s[
-				res['Payload'].v['ParamOffset'],
-				res['Payload'].v['ParamCount']
-			].unpack("v4")
+		os      = smb_fingerprint
+		os_info = "#{os['os']} #{os['sp']} (#{os['lang']})" if os['os'] != "Unknown"
+		shares_info = "#{shares.map{|x| "#{x[0]}"}.join("\x00")}".gsub(/\x00/, ', ')
 
-			data = res['Payload'].to_s[
-				res['Payload'].v['DataOffset'],
-				res['Payload'].v['DataCount']
-			]
-
-			0.upto(lentries - 1) do |i|
-				sname,tmp = data[(i * 20) +  0, 14].split("\x00")
-				stype     = data[(i * 20) + 14, 2].unpack('v')[0]
-				scoff     = data[(i * 20) + 16, 2].unpack('v')[0]
-				if ( lconv != 0)
-					scoff -= lconv
-				end
-				scomm,tmp = data[scoff, data.length - scoff].split("\x00")
-
-				shares << [ sname, share_type(stype), scomm]
-			end
-
-			if not shares.empty?
-				read = false
-				write = false
-				found = true
-				os = smb_fingerprint
-				report_note(
-					:host => ip,
-					:proto => 'tcp',
-					:port => rport,
-					:type => 'smb.shares',
-					:data => { :shares => shares },
-					:update => :unique_data
-				)
-
-				str = "#{shares.map{|x| "#{x[0]}"}.join("\x00")}"
-				list = str.split(/\x00/)
-				str.gsub!(/\x00/, ', ')
-				out = "#{ip}:#{rport}"
-				out << " \\\\#{simple.client.default_domain}" if simple.client.default_domain and simple.client.default_name
-				out << "\\#{simple.client.default_name}" if simple.client.default_name
-
-				desc = " #{os['os']} #{os['sp']}" if os['os'] != "Unknown"
-				desc << " (lang: #{os['lang']})" if os['lang'] != "Unknown"
-				out << desc if desc != nil
-
+		if os_info
 				report_service(
-					:host  => ip,
-					:port  => info[0],
-					:proto => 'tcp',
-					:name  => 'smb',
-					:info  => desc
-				) if desc != nil
+				:host  => ip,
+				:port  => info[0],
+				:proto => 'tcp',
+				:name  => 'smb',
+				:info  => os_info
+			)
+			print_status("#{os_info}: Found #{shares.length.to_s} shares (#{shares_info})")
+		else
+			print_status("Found #{shares.length.to_s} shares (#{shares_info})")
+		end
 
-				print_status(out + ": Found #{shares.length} shares (#{str})")
-				list.each do |x|
-					read,write,type,files = eval_host(ip, x)
-					if(read or write)
-						out = "#{ip}"
-						out << " \\\\#{simple.client.default_domain}" if simple.client.default_domain and  simple.client.default_name
-						out << "\\#{simple.client.default_name}" if simple.client.default_name
-						out << "\\#{x} "
-						out << " (#{type})" if type != nil
-						out << " is readable" if read
-						out << " is writable" if write
-						first = true
-						if datastore['VERBOSE']
-							files.each do |file|
-								if file[0] != '.' and file[0] != '..' and file[0]
-									fa = file[1]['attr']
-									info = file[1]['info']
-									tcr = ::Time.at(::Rex::Proto::SMB::Utils.time_smb_to_unix(info[3], info[2])).strftime("%m-%d-%Y %H:%M:%S")
-									tac = ::Time.at(::Rex::Proto::SMB::Utils.time_smb_to_unix(info[5], info[4])).strftime("%m-%d-%Y %H:%M:%S")
-									twr = ::Time.at(::Rex::Proto::SMB::Utils.time_smb_to_unix(info[7], info[6])).strftime("%m-%d-%Y %H:%M:%S")
-									tch = ::Time.at(::Rex::Proto::SMB::Utils.time_smb_to_unix(info[9], info[8])).strftime("%m-%d-%Y %H:%M:%S")
-									sz = info[12] + info[13]
+		shares
+	end
 
-									case fa
-										when 1
-											fa = "RO"
-										when 2
-											fa = "HIDDEN"
-										when 4
-											fa = "SYS"
-										when 8
-											fa = "VOL"
-										when 16
-											fa = "DIR"
-										when 32
-											fa = "ARC"
-										when 64
-											fa = "DEV"
-										when 128
-											fa = "FILE"
-									end
-									if first
-										out << "\n"
-										out << sprintf("%-6s %-25s ", "Type" , "Name")
-										out << sprintf("%-21s %-21s %-21s %-21s %-15s\n", "Created", "Accessed", "Written", "Changed", "Size")
-										first = false
-									end
+	def get_files_info(ip, rport, shares, info)
+		read  = false
+		write = false
 
-									out << sprintf("%-6s %-25s ", fa, file[0])
-									out << sprintf("%-21s %-21s %-21s %-21s ", tcr,tac,twr,tch)
-									out << "#{sz}\n"
-								end
-							end
-						end
-						print_good(out)
+		list = shares.collect {|e| e[0]}
+		list.each do |x|
+			read,write,type,files = eval_host(ip, x)
+			if read or write
+				header = "#{ip}:#{rport}"
+				if simple.client.default_domain and simple.client.default_name
+					header << " \\\\#{simple.client.default_domain}"
+				end
+				header << "\\#{simple.client.default_name}" if simple.client.default_name
+				header << " (#{type})" if type
+				header << " Readable"  if read
+				header << " Writable"  if write
+
+				tbl = Rex::Ui::Text::Table.new(
+					'Header'  => header,
+					'Indent'  => 1,
+					'Columns' => [ 'Type', 'Name', 'Created', 'Accessed', 'Written', 'Changed', 'Size' ]
+				)
+
+				f_types = {
+					1  => 'RO',  2  => 'HIDDEN', 4  => 'SYS', 8   => 'VOL',
+					16 => 'DIR', 32 => 'ARC',    64 => 'DEV', 128 => 'FILE'
+				}
+
+				files.each do |file|
+					if file[0] and file[0] != '.' and file[0] != '..'
+						info  = file[1]['info']
+						fa    = f_types[file[1]['attr']]       # Item type
+						fname = file[0]                        # Filename
+						tcr   = to_unix_time(info[3], info[2]) # Created
+						tac   = to_unix_time(info[5], info[4]) # Accessed
+						twr   = to_unix_time(info[7], info[6]) # Written
+						tch   = to_unix_time(info[9], info[8]) # Changed
+						sz    = info[12] + info[13]            # Size
+
+						# Filename is too long for the UI table, cut it.
+						fname = "#{fname[0, 35]}..." if fname.length > 35
+
+						tbl << [fa || 'Unknown', fname, tcr, tac, twr, tch, sz]
 					end
 				end
-			end # if shares not empty
-			break if found and rport == 139
+				print_good(tbl.to_s)
+			end
+		end
+	end
+
+	def cleanup
+		datastore['RPORT']     = @rport
+		datastore['SMBDirect'] = @smb_redirect
+	end
+
+	def run_host(ip)
+		@rport        = datastore['RPORT']
+		@smb_redirect = datastore['SMBDirect']
+		shares        = []
+
+		[[139, false], [445, true]].each do |info|
+			print_warning("Options modified: RPORT=#{info[0]}, SMBDirect=#{info[1]}")
+			datastore['RPORT']     = info[0]
+			datastore['SMBDirect'] = info[1]
+
+			begin
+				connect
+				smb_login
+				shares = get_shares(ip, rport, info)
+				unless shares.empty?
+					get_files_info(ip, rport, shares, info)
+					break if rport == 139
+				end
 			rescue ::Interrupt
 				raise $!
-			rescue
-				next if not found and rport == 139
+			rescue ::Rex::Proto::SMB::Exceptions::LoginError,
+				   ::Rex::Proto::SMB::Exceptions::ErrorCode => e
+				print_error(e.message)
+				return if e.message =~ /STATUS_ACCESS_DENIED/
 			rescue ::Rex::ConnectionError,Errno::ECONNRESET,
-				::Rex::Proto::SMB::Exceptions::InvalidType,::Rex::Proto::SMB::Exceptions::ReadPacket,
-				::Rex::Proto::SMB::Exceptions::LoginError,::Rex::Proto::SMB::Exceptions::InvalidCommand,
-				::Rex::Proto::SMB::Exceptions::ErrorCode,::Rex::Proto::SMB::Exceptions::InvalidWordCount,
-				::Rex::Proto::SMB::Exceptions::NoReply => e
-				next if not found and rport == 139		# no results, try again
+				   ::Rex::Proto::SMB::Exceptions::InvalidType,
+				   ::Rex::Proto::SMB::Exceptions::ReadPacket,
+				   ::Rex::Proto::SMB::Exceptions::InvalidCommand,
+				   ::Rex::Proto::SMB::Exceptions::InvalidWordCount,
+				   ::Rex::Proto::SMB::Exceptions::NoReply => e
+				next if not shares.empty? and rport == 139 # no results, try again
 			rescue Errno::ENOPROTOOPT
-				sleep 5
+				select(nil, nil, nil, 5)
 				retry
 			rescue ::Exception => e
-				next if(e.to_s =~ /execution expired/)
-				print_error("Error: '#{ip}' '#{e.class}' '#{e}'")
+				next if e.to_s =~ /execution expired/
+				next if not shares.empty? and rport == 139
+				print_error("Error: '#{ip}' '#{e.class}' '#{e.to_s}'")
+			end
 
-		end # begin
-		return if(rport == 139 and found ) # if we already got results on 139, no need to try 445
-		end # each info
+			# if we already got results on 139, no need to try 445
+			return if (rport == 139 and not shares.empty?)
+		end
 		disconnect
-		return
-	end # run_host
+	end
 end
-
