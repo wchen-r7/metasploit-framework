daa32eb227ca04507cd1d3431dd82f16d05a8691
sinn3r <wei_chen@rapid7.com>
2014-08-28 18:40:11 -0500

    How about this to fix datastore assignments?

--- modules/auxiliary/client/smtp/emailer.rb
+++ modules/auxiliary/client/smtp/emailer.rb
@@ -116,7 +116,7 @@ class Metasploit3 < Msf::Auxiliary
 
     tmp = Dir.tmpdir
 
-    set_mailfrom(from)
+    self.mailfrom = from
 
     msg       = load_file(msg_file)
     email_sig = load_file(sig_file)
@@ -203,7 +203,7 @@ class Metasploit3 < Msf::Auxiliary
 
       mime_msg.from = from
       mime_msg.to = email
-      set_mailto(email.strip)
+      self.mailto = email.strip
       mime_msg.subject = subject
 
       mime_msg.add_part(Rex::Text.encode_base64(email_msg_body, "\r\n"), type, "base64", "inline")
