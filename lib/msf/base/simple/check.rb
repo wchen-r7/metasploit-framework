5a92e9d1503254c52a23cd1f0204ad9ab06a6312
sinn3r <wei_chen@rapid7.com>
2014-09-08 16:00:43 -0500

    proof of concept for fixing rhosts
    It's a mess, but it's a proof of concept so it's okay to be messy
    for now. It demonstrates that it is possible to allow rhosts for
    msfcli.

---
+++ lib/msf/base/simple/check.rb
@@ -0,0 +1,23 @@
+module Msf
+	module Simple
+		class Check
+
+			def initialize(mod, opts)
+				@mod = mod
+				@opts = opts
+			end
+
+			def check
+			end
+
+			def check_simple
+			end
+
+			def check_multiple
+			end
+
+
+
+		end
+	end
+end
